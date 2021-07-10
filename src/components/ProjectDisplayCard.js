009a6ad51b68bfac42d2e62712681d9939e3efe7
teobot <theoclapperton@outlook.com>
2021-07-10 16:16:46 +0100

    Added div fade back

--- src/components/ProjectDisplayCard.js
+++ src/components/ProjectDisplayCard.js
@@ -8,6 +8,8 @@ import useDimensions from "../context/useDimensions";
 
 import { useHistory } from "react-router";
 
+import Fade from "react-reveal/Fade";
+
 export default function ProjectDisplayCard({ project }) {
   const { windowWidth, windowHeight } = useDimensions();
 
@@ -27,65 +29,67 @@ export default function ProjectDisplayCard({ project }) {
   }, [windowWidth]);
 
   return (
-    <div
-      className="projectDisplayCard"
-      ref={ref}
-      onMouseEnter={() => {
-        setMouseHover(true);
-      }}
-      onMouseLeave={() => {
-        setMouseHover(false);
-      }}
-      onClick={() => {
-        history.push(`/p/${slug}`);
-      }}
-      style={{
-        height: divHeight,
-        width: "100%",
-        minWidth: "100%",
-        maxWidth: "100%",
-        marginBottom: 50,
-        cursor: "pointer",
-        borderRadius: 8,
-        position: "relative",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundImage: `url(${image})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <Transition visible={mouseHover} animation="fade" duration={400}>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            width: "100%",
-            position: "absolute",
-            bottom: 0,
-            right: 0,
-            left: 0,
-            height: "35%",
-            padding: 25,
-            borderRadius: 8,
-            background: `linear-gradient(
+    <Fade>
+      <div
+        className="projectDisplayCard"
+        ref={ref}
+        onMouseEnter={() => {
+          setMouseHover(true);
+        }}
+        onMouseLeave={() => {
+          setMouseHover(false);
+        }}
+        onClick={() => {
+          history.push(`/p/${slug}`);
+        }}
+        style={{
+          height: divHeight,
+          width: "100%",
+          minWidth: "100%",
+          maxWidth: "100%",
+          marginBottom: 50,
+          cursor: "pointer",
+          borderRadius: 8,
+          position: "relative",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <Transition visible={mouseHover} animation="fade" duration={400}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              left: 0,
+              height: "35%",
+              padding: 25,
+              borderRadius: 8,
+              background: `linear-gradient(
                 180deg,
                 rgba(255, 255, 255, 0) 0%,
                 rgba(0, 0, 0, 0.75) 50%,
                 rgba(0, 0, 0, 0.95) 75%,
                 rgba(0, 0, 0, 1) 100%
               )`,
-          }}
-        >
-          <div style={{ bottom: 25, position: "absolute" }}>
-            <Header size="large" inverted>
-              <Header.Content>
-                {header}
-                <Header.Subheader>{header_sub}</Header.Subheader>
-              </Header.Content>
-            </Header>
+            }}
+          >
+            <div style={{ bottom: 25, position: "absolute" }}>
+              <Header size="large" inverted>
+                <Header.Content>
+                  {header}
+                  <Header.Subheader>{header_sub}</Header.Subheader>
+                </Header.Content>
+              </Header>
+            </div>
           </div>
-        </div>
-      </Transition>
-    </div>
+        </Transition>
+      </div>
+    </Fade>
   );
 }

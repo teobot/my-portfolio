a902570d686f57cb7b448d4689a0e557e747c9b1
teobot <theoclapperton@outlook.com>
2021-07-12 12:12:46 +0100

    Fixed no loading divs

--- src/components/ProjectDisplayCard.js
+++ src/components/ProjectDisplayCard.js
@@ -8,7 +8,7 @@ import useDimensions from "../context/useDimensions";
 
 import { useHistory } from "react-router";
 
-import Fade from "react-reveal/Fade";
+import { useSpring, animated } from 'react-spring'
 
 export default function ProjectDisplayCard({ project }) {
   const { windowWidth, windowHeight } = useDimensions();
@@ -22,6 +22,8 @@ export default function ProjectDisplayCard({ project }) {
 
   const ref = useRef();
 
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+
   useEffect(() => {
     if (ref.current.clientWidth) {
       setDivHeight(ref.current.clientWidth / 2);
@@ -29,67 +31,65 @@ export default function ProjectDisplayCard({ project }) {
   }, [windowWidth]);
 
   return (
-    <Fade>
-      <div
-        className="projectDisplayCard"
-        ref={ref}
-        onMouseEnter={() => {
-          setMouseHover(true);
-        }}
-        onMouseLeave={() => {
-          setMouseHover(false);
-        }}
-        onClick={() => {
-          history.push(`/p/${slug}`);
-        }}
-        style={{
-          height: divHeight,
-          width: "100%",
-          minWidth: "100%",
-          maxWidth: "100%",
-          marginBottom: 50,
-          cursor: "pointer",
-          borderRadius: 8,
-          position: "relative",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundImage: `url(${image})`,
-          backgroundSize: "cover",
-        }}
-      >
-        <Transition visible={mouseHover} animation="fade" duration={400}>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
-              position: "absolute",
-              bottom: 0,
-              right: 0,
-              left: 0,
-              height: "35%",
-              padding: 25,
-              borderRadius: 8,
-              background: `linear-gradient(
+    <div
+      ref={ref}
+      className="projectDisplayCard"
+      onMouseEnter={() => {
+        setMouseHover(true);
+      }}
+      onMouseLeave={() => {
+        setMouseHover(false);
+      }}
+      onClick={() => {
+        history.push(`/p/${slug}`);
+      }}
+      style={{
+        height: divHeight,
+        width: "100%",
+        minWidth: "100%",
+        maxWidth: "100%",
+        marginBottom: 50,
+        cursor: "pointer",
+        borderRadius: 8,
+        position: "relative",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <Transition visible={mouseHover} animation="fade" duration={400}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            left: 0,
+            height: "35%",
+            padding: 25,
+            borderRadius: 8,
+            background: `linear-gradient(
                 180deg,
                 rgba(255, 255, 255, 0) 0%,
                 rgba(0, 0, 0, 0.75) 50%,
                 rgba(0, 0, 0, 0.95) 75%,
                 rgba(0, 0, 0, 1) 100%
               )`,
-            }}
-          >
-            <div style={{ bottom: 25, position: "absolute" }}>
-              <Header size="large" inverted>
-                <Header.Content>
-                  {header}
-                  <Header.Subheader>{header_sub}</Header.Subheader>
-                </Header.Content>
-              </Header>
-            </div>
+          }}
+        >
+          <div style={{ bottom: 25, position: "absolute" }}>
+            <Header size="large" inverted>
+              <Header.Content>
+                {header}
+                <Header.Subheader>{header_sub}</Header.Subheader>
+              </Header.Content>
+            </Header>
           </div>
-        </Transition>
-      </div>
-    </Fade>
+        </div>
+      </Transition>
+    </div>
   );
 }

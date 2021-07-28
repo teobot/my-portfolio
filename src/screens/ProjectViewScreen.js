d11ffdd5fc504fecbc6b6d06ee1da055eb5808c9
teobot <theoclapperton@outlook.com>
2021-07-28 23:47:32 +0100

    Updated UI on code syntax

--- src/screens/ProjectViewScreen.js
+++ src/screens/ProjectViewScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/first */
 import React, { useContext, useEffect, useState } from "react";
 
 import { useHistory, useParams } from "react-router";
@@ -5,7 +6,7 @@ import { useHistory, useParams } from "react-router";
 import {
   Divider,
   Menu,
-  Icon,
+  Table,
   Image,
   Header,
   List,
@@ -20,12 +21,14 @@ import { ThemeContext } from "../context/ThemeContext";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 
+// agate, androidstudio, hybrid
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { agate as SyntaxTheme } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 import Loader from "react-loader-spinner";
 
 import NavBar from "../components/NavBar";
+import MorphBlob from "../components/MorphBlob";
 
 const EXCLUDE_TAGS = ["Netlify Status", "Version"];
 
@@ -68,142 +71,151 @@ export default function Project() {
           alignItems: "center",
         }}
       >
+        <MorphBlob />
         <Loader
           type="Grid"
           color="#00BFFF"
           height={windowWidth / 8}
           width={windowWidth / 8}
+          style={{ zIndex: 3 }}
         />
       </div>
     );
   }
 
   return (
-    <>
+    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
       <NavBar project_title={project.header} />
-      <div
+
+      <MorphBlob />
+
+      <Container
         style={{
-          backgroundColor: theme.backgroundColor,
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
           padding: windowWidth / 50,
+          marign: windowWidth / 50,
+          zIndex: 1,
+          overflowY: "auto",
         }}
       >
-        <Container
-          style={{
-            boxShadow:
-              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-            padding: windowWidth / 40,
+        <ReactMarkdown
+          remarkPlugins={[gfm]}
+          components={{
+            img: ({ node, ...props }) => {
+              if (EXCLUDE_TAGS.includes(props.alt)) {
+                return <Image {...props} spaced />;
+              } else {
+                return (
+                  <Image
+                    alt=""
+                    {...props}
+                    fluid
+                    size="massive"
+                    centered
+                    style={{ margin: "10px 0px 10px 0px" }}
+                  />
+                );
+              }
+            },
+            h1: ({ node, ...props }) => (
+              <>
+                <Header {...props} as="h1" inverted={darkMode} />
+                <Divider />
+              </>
+            ),
+            h2: ({ node, ...props }) => (
+              <Header {...props} as="h2" dividing inverted={darkMode} />
+            ),
+            h3: ({ node, ...props }) => (
+              <Header {...props} as="h3" inverted={darkMode} />
+            ),
+            h4: ({ node, ...props }) => (
+              <Header {...props} as="h4" inverted={darkMode} />
+            ),
+            h5: ({ node, ...props }) => (
+              <Header {...props} as="h5" inverted={darkMode} />
+            ),
+            h6: ({ node, ...props }) => (
+              <Header {...props} as="h6" inverted={darkMode} />
+            ),
+            ol: ({ node, ...props }) => {
+              return <ol {...props} style={{ color: theme.text }} />;
+            },
+            ul: ({ node, ...props }) => {
+              return (
+                <List
+                  as="ul"
+                  {...props}
+                  inverted={darkMode}
+                  style={{ color: theme.text }}
+                />
+              );
+            },
+            code: ({ node, ...props }) => {
+              if (props.inline) {
+                return (
+                  <code
+                    style={{
+                      backgroundColor: "#f6f8fa",
+                      color: "black",
+                      padding: "2px 5px 2px 5px",
+                      margin: "2px 5px 2px 5px",
+                      borderRadius: 5,
+                    }}
+                    {...props}
+                  />
+                );
+              } else {
+                return <SyntaxHighlighter {...props} style={SyntaxTheme} />;
+              }
+            },
+            strong: ({ node, ...props }) => {
+              return (
+                <strong
+                  {...props}
+                  style={{
+                    color: theme.text,
+                    fontSize: "calc(14px + 0.15vw)",
+                    fontWeight: "bolder",
+                  }}
+                />
+              );
+            },
+            em: ({ node, ...props }) => {
+              return <em {...props} style={{ color: theme.text }} />;
+            },
+            p: ({ node, ...props }) => {
+              return (
+                <p
+                  {...props}
+                  style={{
+                    color: theme.text,
+                    fontSize: "calc(12px + 0.15vw)",
+                  }}
+                />
+              );
+            },
+            a: ({ node, ...props }) => {
+              return (
+                // eslint-disable-next-line jsx-a11y/anchor-has-content
+                <a
+                  {...props}
+                  style={{
+                    color: "#2185d0",
+                    fontWeight: "bolder",
+                    textDecoration: "underline",
+                  }}
+                />
+              );
+            },
+            table: ({ node, ...props }) => {
+              return <Table {...props} striped stackable inverted />;
+            },
           }}
-        >
-          <ReactMarkdown
-            remarkPlugins={[gfm]}
-            components={{
-              img: ({ node, ...props }) => {
-                if (EXCLUDE_TAGS.includes(props.alt)) {
-                  return <Image {...props} spaced />;
-                } else {
-                  return (
-                    <Image
-                      alt=""
-                      {...props}
-                      fluid
-                      size="massive"
-                      centered
-                      style={{ margin: "10px 0px 10px 0px" }}
-                    />
-                  );
-                }
-              },
-              h1: ({ node, ...props }) => (
-                <>
-                  <Header {...props} as="h1" inverted={darkMode} />
-                  <Divider />
-                </>
-              ),
-              h2: ({ node, ...props }) => (
-                <Header {...props} as="h2" dividing inverted={darkMode} />
-              ),
-              h3: ({ node, ...props }) => (
-                <Header {...props} as="h3" inverted={darkMode} />
-              ),
-              h4: ({ node, ...props }) => (
-                <Header {...props} as="h4" inverted={darkMode} />
-              ),
-              h5: ({ node, ...props }) => (
-                <Header {...props} as="h5" inverted={darkMode} />
-              ),
-              h6: ({ node, ...props }) => (
-                <Header {...props} as="h6" inverted={darkMode} />
-              ),
-              ol: ({ node, ...props }) => {
-                return (
-                  <List
-                    as="ol"
-                    {...props}
-                    ordered
-                    inverted={darkMode}
-                    style={{ color: theme.text }}
-                  />
-                );
-              },
-              ul: ({ node, ...props }) => {
-                return (
-                  <List
-                    as="ul"
-                    {...props}
-                    inverted={darkMode}
-                    style={{ color: theme.text }}
-                  />
-                );
-              },
-              code: ({ node, ...props }) => {
-                if (props.inline) {
-                  return (
-                    <code
-                      style={{
-                        backgroundColor: "#f6f8fa",
-                        color: "black",
-                        padding: "2px 5px 2px 5px",
-                        margin: "2px 5px 2px 5px",
-                        borderRadius: 5,
-                      }}
-                      {...props}
-                    />
-                  );
-                } else {
-                  return <SyntaxHighlighter {...props} style={docco} />;
-                }
-              },
-              strong: ({ node, ...props }) => {
-                return (
-                  <strong
-                    {...props}
-                    style={{
-                      color: theme.text,
-                      fontSize: "calc(14px + 0.15vw)",
-                      fontWeight: "bolder",
-                    }}
-                  />
-                );
-              },
-              em: ({ node, ...props }) => {
-                return <em {...props} style={{ color: theme.text }} />;
-              },
-              p: ({ node, ...props }) => {
-                return (
-                  <p
-                    {...props}
-                    style={{
-                      color: theme.text,
-                      fontSize: "calc(12px + 0.15vw)",
-                    }}
-                  />
-                );
-              },
-            }}
-            children={readme}
-          />
-        </Container>
-      </div>
-    </>
+          children={readme}
+        />
+      </Container>
+    </div>
   );
 }
